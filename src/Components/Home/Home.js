4bacafd5a512d689dbdec0947b932ca8de119ccd
NA-Siddiky <siddiky.academic@gmail.com>
2021-06-20 14:55:13 +0600

    ser register in home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Register from '../Login/Register/Register';
 
 const Home = () => {
     return (
         <div>
             <h1>This is home</h1>
+            <Register></Register>
         </div>
     );
 };
